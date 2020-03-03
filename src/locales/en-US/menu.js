f4c1f0ac3f18fc9da7de4c33f382deb08a3531b2
yangbo <bo.yang@inskylab.com>
2020-03-03 18:35:49 +0800

    无人机展示及实时位置更新

---
+++ src/locales/en-US/menu.js
@@ -0,0 +1,8 @@
+export default {
+  'menu.users': 'User Management',
+  'menu.users.userslist': 'User List',
+  'menu.users.roleslist': 'Role List',
+  'menu.devices': 'Device Management',
+  'menu.fileupload': 'File Upload',
+  'menu.flightstatus': 'Flight Status',
+};
