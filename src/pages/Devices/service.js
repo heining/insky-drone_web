0e70c304d6c337368c2b206ff4b1910aaa866b70
yangbo <bo.yang@inskylab.com>
2020-03-18 19:02:13 +0800

    飞行状态功能交互优化完成

--- src/pages/Devices/service.js
+++ src/pages/Devices/service.js
@@ -4,3 +4,8 @@ import request from '@/utils/request';
 export async function getDevice() {
   return request('/drone/v1/device');
 }
+
+//通过id获取设备数据详情
+export async function getDeviceDataId() {
+  return request('http://api.inskylab.cn/drone/v1/device/data');
+}
